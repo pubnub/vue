c600674bb95f71ae4bf259dd13bb633fe6e05f93
manuelfernando <manuelcamaron@hotmail.com>
2018-01-22 20:31:05 -0500

    add module getMessage

---
+++ src/modules/getMessage.js
@@ -0,0 +1,79 @@
+/**
+ * Add a channel to the set of data
+ *
+ * @param {string} channel
+ * @returns {boolean}
+ */
+function init(channel) {
+  if (channel in this._data.messages) {
+    return false;
+  } else {
+    this._data.messages[channel] = [];
+    return true;
+  }
+}
+
+/**
+ * Emit a message through a callback and update the set of data
+ *
+ * @param {string} channel
+ * @param {object} message
+ */
+function emit(channel, message) {
+  let messages = this._data.messages[channel];
+  let keepMessages = this._keepMessages[channel];
+
+  messages.push(message);
+
+  if (keepMessages && messages.length > keepMessages) {
+    this._data.messages = messages.slice(messages.length - keepMessages);
+  }
+
+  this._broadcast.emit('message', channel, message);
+}
+
+/**
+ * Get to receive messages from a channel through a callback
+ *
+ * @param {string} channel
+ * @param {function} callback
+ * @returns {[]}
+ */
+export function getMessage(channel) {
+  let callback;
+  let keepMessages = 100;
+
+  if (arguments.length === 2 && typeof arguments[1] === 'function') {
+    callback = arguments[1];
+  } else if (arguments.length === 2 && typeof arguments[1] === 'number') {
+    keepMessages = arguments[1];
+  } else if (arguments.length === 3) {
+    callback = arguments[1];
+    keepMessages = arguments[2];
+  }
+
+  if (init.call(this, channel)) {
+    this._keepMessages[channel] = keepMessages;
+    // this._autoload.getHistory(channel, callback);
+  }
+
+  this._broadcast.message(channel, callback);
+
+  if (!this._listener.message) {
+    this._listener.message = (message) => {
+      if (message.subscription && this._broadcast.isSubscribe('message', message.subscription)) {
+        emit.call(this, message.subscription, message);
+      }
+
+      if (message.channel && this._broadcast.isSubscribe('message', message.channel)) {
+        emit.call(this, message.channel, message);
+      }
+    };
+  }
+
+  if (this._data.messages && this._data.messages[channel]) {
+    return this._data.messages[channel];
+  } else {
+    return [];
+  }
+}
---
+++ test/modules/getMessages.test.js
@@ -0,0 +1,59 @@
+/* global describe, beforeEach, it, before, after */
+/* eslint no-console: 0 */
+import { assert } from 'chai';
+import { getMessage } from '../../src/modules';
+import Broadcast from '../../src/broadcast';
+
+let mock = {};
+
+console.log(getMessage);
+
+describe('getMessage', () => {
+  before((done) => {
+    mock._broadcast = new Broadcast();
+    mock._listener = {};
+    mock._data = { messages: {} };
+    mock._keepMessages = {};
+    mock.getMessage = getMessage.bind(mock);
+    done();
+  });
+
+  it('should allocate a channel', (done) => {
+    mock.getMessage('channel1');
+    assert.deepEqual(mock._data, { messages: { channel1: [] } }, 'channel1 was not allocated');
+    done();
+  });
+
+  it('should set to 100 the keepMessages', (done) => {
+    assert.deepEqual(mock._keepMessages, { channel1: 100 }, 'it was not set 100 by default');
+    done();
+  });
+
+  it('should set a custom value to the keepMessages', (done) => {
+    mock.getMessage('channel2', 50);
+    assert.deepEqual(mock._keepMessages, { channel1: 100, channel2: 50 }, 'it was not set 50 to channel2');
+    done();
+  });
+
+  it('should stack a message', (done) => {
+    mock._listener.message({ channel: 'channel1', message: 'message #1'});
+    assert.deepEqual(mock._data.messages.channel1, [{ channel: 'channel1', message: 'message #1'}], 'it was not stacked the message');
+    done();
+  });
+
+  it('should broadcast a message', (done) => {
+    mock.getMessage('channel3', (msg) => {
+      assert.deepEqual(msg, { channel: 'channel3', message: 'message #1'}, 'it was not the message expected');
+      done();
+    });
+
+    mock._listener.message({ channel: 'channel3', message: 'message #1'});
+  });
+
+  it('should change the stacks', (done) => {
+    assert.lengthOf(mock._data.messages.channel1, 1);
+    assert.lengthOf(mock._data.messages.channel2, 0);
+    assert.lengthOf(mock._data.messages.channel3, 1);
+    done();
+  });
+});
