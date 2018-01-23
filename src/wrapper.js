c1c981dcb6be6ea3769d583d9dfd863ea2efb5e9
manuelfernando <manuelcamaron@hotmail.com>
2018-01-23 17:54:03 -0500

    elevate function wrapper to a class

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,4 +1,7 @@
-import config from '../config.json';
+import { getMessage, getPresence, getStatus } from './modules';
+import Broadcast from './broadcast';
+import Autoload from './autoload';
+import config from '../config';
 
 /**
  * Wrap a PubNub's attributes
@@ -26,12 +29,44 @@ function wrapMethod(OriginalInstance, wrappedInstance, methodName) {
  * @param originalInstance
  * @param wrappedInstance
  */
-export default function (originalInstance, wrappedInstance) {
-  config.attributes_to_delegate.forEach((attribute) => {
-    wrapAttribute(originalInstance, wrappedInstance, attribute);
-  });
 
-  config.methods_to_delegate.forEach((method) => {
-    wrapMethod(originalInstance, wrappedInstance, method);
-  });
+export default class {
+  constructor(originalInstance) {
+    this._originInstance = originalInstance;
+    this._broadcast = new Broadcast();
+    this._autoload = new Autoload();
+    this._listener = {};
+    this._data = { messages: {}, presence: {}, status: {} };
+    this._keepMessages = {};
+
+    config.attributes_to_delegate.forEach((attribute) => {
+      wrapAttribute(originalInstance, wrappedInstance, attribute);
+    });
+
+    config.methods_to_delegate.forEach((method) => {
+      wrapMethod(originalInstance, wrappedInstance, method);
+    });
+
+    this.getMessage = getMessage.bind(this);
+    this.getPresence = getPresence.bind(this);
+    this.getStatus = getStatus.bind(this);
+
+    this.addListener(this._listener);
+  }
+
+  subscribe(args) {
+    this.getOriginalInstance().subscribe(args);
+  }
+
+  unsubscribe(args) {
+    this.getOriginalInstance().unsubscribe(args);
+  }
+
+  getOriginalInstance() {
+    if (this._originInstance) {
+      return this._originInstance;
+    } else {
+      throw new ReferenceError('Pubnub default instance is not initialized yet');
+    }
+  }
 }
