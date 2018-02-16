834eed9364917509d9a4bff26820b7f12f175c80
manuelfernando <manuelcamaron@hotmail.com>
2018-02-15 19:52:31 -0500

    improve initializer

--- src/pubnub-vue.js
+++ src/pubnub-vue.js
@@ -1,23 +1,27 @@
 import PubNub from 'pubnub';
 import Wrapper from './wrapper';
-import config from '../config';
+import config from '../config.json';
 
 let instances = {};
 
 export default class PubNubVue {
-  static getInstance(initConfig, instanceName) {
+  static getInstance(instanceName, initConfig) {
     if (typeof instanceName === 'string' && instanceName.length > 0) {
-      if (instanceName in instanceName) {
+      if (instanceName in instances) {
         return instances[instanceName];
       } else {
         instances[instanceName] = new Wrapper(new PubNub(initConfig));
+        return instances[instanceName];
       }
+    } else if (!(config.default_instance_name in instances)) {
+      instances[config.default_instance_name] = new Wrapper(new PubNub(initConfig));
+      return instances[config.default_instance_name];
     } else {
-      return this.getOriginalInstance();
+      return this.getDefaultInstance();
     }
   }
 
-  static getOriginalInstance() {
+  static getDefaultInstance() {
     return instances[config.default_instance_name];
   }
 }
